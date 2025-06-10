591e1aa640a9d40b3ea8576e2a5ea95e03875968
joeybail96 <joey.bail@utah.edu>
2025-06-10 12:38:14 -0600

    adding functionality to plot timeseries of specified species, grab stats, and zoom in on peak concentration times

--- NACHTT.py
+++ NACHTT.py
@@ -1,5 +1,6 @@
-from NACHTT_utils import Plotter
+from NACHTT_utils import Plotter, Processor
 plotter = Plotter()
+processor = Processor()
 
 import xarray as xr
 
@@ -21,20 +22,33 @@ base_nc = xr.open_dataset(base_nc_file)
 
 # Plot NACHTT diurnal variation in following:
     # ClNO2
-
 ClNO2_diurnal_fig = '../figures/ClNO2_diurnal.png'
 plotter.plot_diurnal_variation(nachtt_nc, 'ClNO2_pptv', scale_factor=1, time_var='time', local_offset=-7, ylabel='Observed (pptv)',
                                second_dataset=base_nc, second_variable_name='ClNO2', second_scale_factor=1e12, second_time_var='time_UTC', second_ylabel='Modeled (pptv)',
                                primary_ylim=(-0.2,50), secondary_ylim=(-0.2,10), fig_save_path=ClNO2_diurnal_fig)
 
 
-Cl2_diurnal_fig = '../figures/Cl2_diurnal.png'
-plotter.plot_diurnal_variation(nachtt_nc, 'Cl2_pptv', scale_factor=1, time_var='time', local_offset=-7, ylabel='Observed (pptv)',
-                               second_dataset=base_nc, second_variable_name='Cl2', second_scale_factor=1e12, second_time_var='time_UTC', second_ylabel='Modeled (pptv)',
-                               primary_ylim=(-5.99,5.99), secondary_ylim=(-0.02, 0.52), fig_save_path=Cl2_diurnal_fig)
+
+# Plot NACHTT full time series for following species and identify peaks
+    # ClNO2, N2O5, O3, NO2, NO, NO3, pCl, pNO3, Cl2, HONO, HCl, Temp, Pressure, wind
+plotter.plot_time_series(nachtt_nc, 'ClNO2_pptv', scale_factor=1, time_var='time', local_offset=-7, ylabel='Observed (pptv)', average_interval='30min')
 
 
-Cl_diurnal_fig = '../figures/Cl_diurnal.png'
-plotter.plot_diurnal_variation(nachtt_nc, 'AMS_pCl_ugm3', scale_factor=1, time_var='time', local_offset=-7, ylabel='Observed (ug m^-3)',
-                               second_dataset=base_nc, second_variable_name='Cl', second_scale_factor=1, second_time_var='time_UTC', second_ylabel='Modeled (ug m^-3)',
-                               primary_ylim=(-0.01,0.1), secondary_ylim=(-0.01,0.1), fig_save_path=Cl_diurnal_fig)
+
+stats, peaks = processor.get_peak_n_values(nachtt_nc, 'ClNO2_pptv', average_interval='30min')
+peak_times = peaks.time
+peak_xlims = processor.get_xlim_from_peaks(peak_times, hours_before=12, hours_after=12)
+
+for xlim in peak_xlims:
+    plotter.plot_time_series(
+        dataset=nachtt_nc,
+        variable='ClNO2_pptv',
+        xlim=xlim,
+        ylabel='Observed (pptv)',
+        average_interval='30min'
+    )
+
+
+
+
+
--- NACHTT_utils.py
+++ NACHTT_utils.py
@@ -59,3 +59,114 @@ class Plotter:
             plt.close(fig)  # Close the figure explicitly to prevent auto-display
         else:
             plt.show()
+            
+            
+            
+    def plot_time_series(self, dataset, variable, scale_factor=1, time_var='time', local_offset=0, ylabel='Observed', average_interval=None, xlim=None, ylim=None):
+        """
+        Creates a time series plot from the given dataset, with optional averaging.
+    
+        Parameters:
+        - dataset: Input data structure (xarray Dataset, pandas DataFrame, or similar) containing time and variable data.
+        - variable: Name of the variable to plot.
+        - scale_factor: Value to scale the variable (default is 1, no scaling).
+        - time_var: Name of the time variable in the dataset.
+        - local_offset: Time zone offset in hours to apply to the time variable.
+        - ylabel: Label for the y-axis.
+        - average_interval: Pandas offset string (e.g., '10min', '1H') for averaging (optional).
+        """
+        import pandas as pd
+        import matplotlib.pyplot as plt
+    
+        # Extract time and variable, apply scale factor
+        time = pd.to_datetime(dataset[time_var].values) + pd.to_timedelta(local_offset, unit='h')
+        data = dataset[variable].values * scale_factor
+    
+        # Create a pandas Series for easy resampling if averaging is requested
+        series = pd.Series(data, index=time)
+    
+        if average_interval:
+            # Resample and average
+            series = series.resample(average_interval).mean()
+    
+        # Create the plot
+        plt.figure(figsize=(12, 6))
+        plt.plot(series.index, series.values, label=variable, color='tab:blue')
+        
+        # Apply x-axis limits if provided
+        if xlim:
+            plt.xlim(pd.to_datetime(xlim[0]), pd.to_datetime(xlim[1]))
+            
+        if ylim:
+            plt.ylim(pd.to_datetime(ylim[0]), pd.to_datetime(ylim[1]))
+            
+        plt.xlabel('Time')
+        plt.ylabel(ylabel)
+        plt.title(f'Time Series of {variable}' + (f' (Averaged: {average_interval})' if average_interval else ''))
+        plt.grid(True)
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+        
+        
+        
+class Processor:
+    def __init__(self):
+        pass
+
+    def get_peak_n_values(self, dataset, var_name, average_interval=None, n=5, time_var='time', local_offset=0):
+        """
+        Returns a DataFrame of the top n highest points (time and value) of a variable in the dataset,
+        optionally averaging by a specified pandas offset string interval.
+        Also returns summary statistics of the entire dataset variable (after averaging if applied).
+
+        Parameters:
+        - dataset: xarray Dataset loaded from a NetCDF file.
+        - var_name: Name of the variable to analyze.
+        - average_interval: pandas offset string for resampling and averaging (e.g., '10min', '1H'). If None, no averaging.
+        - n: Number of top points to return (default 5).
+        - time_var: Name of the time coordinate variable (default 'time').
+        - local_offset: Timezone offset in hours to apply to the time coordinate (default 0).
+
+        Returns:
+        - summary_stats: dict with keys ['mean', 'median', 'std', 'min', 'max'] for the entire data
+        - top_n_df: pandas DataFrame with columns ['time', 'value'] for the top n points
+        """
+        # Convert time coordinate to pandas datetime with local offset
+        time = pd.to_datetime(dataset[time_var].values) + pd.to_timedelta(local_offset, unit='h')
+        data = dataset[var_name].values
+
+        # Create pandas Series for easy resampling and handling time index
+        series = pd.Series(data, index=time)
+
+        if average_interval:
+            # Resample with mean, ignoring NaNs where possible
+            series = series.resample(average_interval).mean().dropna()
+
+        # Compute summary statistics on the whole series
+        summary_stats = {
+            'mean': series.mean(),
+            'median': series.median(),
+            'std': series.std(),
+            'min': series.min(),
+            'max': series.max()
+        }
+
+        # Get top n values and corresponding times
+        top_n = series.nlargest(n)
+        top_n_df = pd.DataFrame({'time': top_n.index, 'value': top_n.values}).reset_index(drop=True)
+
+        return summary_stats, top_n_df
+    
+    
+    def get_xlim_from_peaks(self, peak_times, hours_before=12, hours_after=12):
+        """
+        Given a DataFrame with a 'time' column, return a list of (start, end) tuples
+        where start = peak_time - hours_before, end = peak_time + hours_after.
+        """
+        return [(t - pd.Timedelta(hours=hours_before), t + pd.Timedelta(hours=hours_after)) for t in peak_times]
+    
+    
+    
+    
+    
